54cd81142b263cf815e6f0552e86b1d7416aeb94
WellsGuan <160977744+WellsGuan@users.noreply.github.com>
2024-04-01 14:08:33 -0500

    Delete Tex directory

--- Tex/bin.sh
+++
@@ -1,9 +0,0 @@
-for file in ./*
-do
-    find . -name '*.aux' -delete
-    find . -name '*.bcf' -delete
-    find . -name '*.out' -delete
-    find . -name '*.log' -delete
-    find . -name '*.xml' -delete
-    find . -name '*.gz' -delete
-done
